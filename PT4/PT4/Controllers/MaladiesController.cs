8b96db9ca2780d4e7462e6b5c57cba865c833fcc
dorian vidal <louloudorian@hotmail.fr>
2022-02-18 12:13:07 +0100

    Création d'une maladie et fonction pour lister les maladies

---
+++ PT4/PT4/Controllers/MaladiesController.cs
@@ -0,0 +1,41 @@
+﻿using PT4.Model.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT4.Controllers
+{
+    internal class MaladiesController
+    {
+        private IMaladieRepository _maladieRepo;
+
+        public MaladiesController(IMaladieRepository maladieRepo)
+        {
+            _maladieRepo = maladieRepo;
+        }
+
+
+        public void CreerMaladie(string nomMaladie)
+        {
+            MALADIE maladie = _maladieRepo.FindWhere(m => m.NOMMALADIE == nomMaladie).FirstOrDefault();
+            if (maladie != null)
+            {
+                throw new Exception("Une maladie porte déjà ce nom");
+            }
+            maladie = new MALADIE
+            {
+                NOMMALADIE = nomMaladie
+            };
+
+            _maladieRepo.Insert(maladie);
+            _maladieRepo.Save();
+        }
+
+        public IEnumerable<MALADIE> ListerMaladies()
+        {
+            return _maladieRepo.FindAll();
+        }
+    }
+}
