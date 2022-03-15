05b882c2ba90d78054d5707057e32b13f932d233
Jeulin Elian <elian.jeulin@etu.u-bordeaux.fr>
2022-03-15 10:49:50 +0100

    TU

--- PT4/TestProjetVeto/TestAnimal.cs
+++ PT4/TestProjetVeto/TestAnimal.cs
@@ -4,6 +4,7 @@ using PT4;
 using PT4.Model.dal;
 using PT4.Controllers;
 using System.Linq;
+using System;
 
 namespace TestProjetVeto
 {
@@ -14,33 +15,10 @@ namespace TestProjetVeto
         private IGenericRepository<ANIMAL> _animalRepo;
         private IGenericRepository<CLIENT> _clientRepo;
         private AnimalController _animalController;
+        public CLIENT clientTest;
+        public ANIMAL animalTest;
 
-        CLIENT clientTest = new CLIENT
-        {
-            NOMCLIENT = "Test",
-            PRENOMCLIENT = "Test",
-            NUMERO = "Test",
-            EMAIL = "Test"
-        };
-
-        ANIMAL animalTest = new ANIMAL
-        {
-            client = clientTest,
-            nomEspece = "chat",
-            nomRace = "tigre",
-            nomAnimal = "Test",
-            dateNaissance = 10 / 03 / 2009,
-            taille = 18,
-            poids = 7
-        };
-
-        RENDEZVOUS rdvTest = new RENDEZVOUS
-        {
-            client = clientTest,
-            dateRdv = 10/03/2050 10:00:00,
-            raison = "Mon chat ne veut plus manger",
-            finRdv = 10/03/2050 10:30:00
-        });
+        
 
         [TestInitialize]
         public void TestInitialize()
@@ -61,6 +39,25 @@ namespace TestProjetVeto
                 _clientRepo = provider.GetRequiredService<IGenericRepository<CLIENT>>();
                 _animalController = provider.GetRequiredService<AnimalController>();
             }
+
+            clientTest = new CLIENT
+            {
+                NOMCLIENT = "Test",
+                PRENOMCLIENT = "Test",
+                NUMERO = "Test",
+                EMAIL = "Test"
+            };
+
+            animalTest = new ANIMAL
+            {
+                CLIENT = clientTest,
+                NOMESPECE = "chat",
+                NOMRACE = "tigre",
+                NOMANIMAL = "Test",
+                DATENAISSANCE = new DateTime(2009, 3, 10, 10, 0, 0),
+                TAILLE = 18,
+                POIDS = 7
+            };
         }
 
         [TestMethod]
@@ -74,9 +71,9 @@ namespace TestProjetVeto
 
             Assert.AreEqual(req1.Count(), 0); // Test si aucun animal de ce nom dans la base existe déjà
 
-            _animalController.CreerAnimal(clientTest, animalTest.nomEspece, animalTest.nomRace, animalTest.nomAnimal, animalTest.dateNaissance, animalTest.taille, animalTest.poids);
-            var req2 = from animal in IAnimalRepository
-                       where animal.nomAnimal == animal1.nomAnimal
+            _animalController.CreerAnimal(clientTest, animalTest.NOMESPECE, animalTest.NOMRACE, animalTest.NOMANIMAL, animalTest.DATENAISSANCE, animalTest.TAILLE, animalTest.POIDS);
+            var req2 = from animal in _animalRepo.FindAll()
+                       where animal.NOMANIMAL == animalTest.NOMANIMAL
                        select animal;
 
             Assert.AreEqual(req2.Count(), 1); // Test l'insertion d'un animal dans la base
@@ -89,63 +86,5 @@ namespace TestProjetVeto
             }
             _animalRepo.Save();
         }
-
-        [TestMethod]
-        public void TestCreerClient()
-        {
-            var req1 = from client in IClientRepository
-                       where client.nom == clientTest.nom
-                       select client;
-
-            Assert.AreEqual(req1.Count(), 0); // Test si aucun client dans la base de ce nom existe déjà
-
-            CreerClient(clientTest.nom, clientTest.prenom, clientTest.numero, clientTest.email);
-            var req2 = from client in IClientRepository
-                       where client.nomAnimal == clientTest.nomAnimal
-                       select client;
-
-            Assert.AreEqual(req2.Count(), 1); // Test l'insertion d'un client dans la base
-
-            foreach (CLIENT cli in req2)
-            {
-                Requêtes.baseMusique.ABONNÉS.Remove(cli);
-            }
-            Requêtes.baseMusique.SaveChanges();
-            _animalRepository.Save();
-        }
-
-        [TestMethod]
-        public void TestCreerRDV()
-        {
-            var req1 = from rdv in IRendezVousRepository
-                       where rdv.dateRdv == rdvTest.dateRdv
-                       select rdv;
-
-            Assert.AreEqual(req1.Count(), 0); // Test si aucun rdv dans la base de ce type existe déjà
-
-            CreerRendezVous(clientTest, rdvTest.dateRdv , rdvTest.raison, rdvTest.finRdv);
-            var req2 = from rdv in IRendezVousRepository
-                       where rdv.dateRdv == rdvTest.dateRdv
-                       select rdv;
-
-            Assert.AreEqual(req2.Count(), 1); // Test l'insertion d'un rendez vous dans la base
-
-            foreach (RENDEZVOUS rdv in req2)
-            {
-                Requêtes.baseMusique.ABONNÉS.Remove(rdv);
-            }
-            Requêtes.baseMusique.SaveChanges();
-            _animalRepository.Save();
-        }
-
-        [TestMethod]
-        public void Test2()
-        {
-
-        }
-
-
-
-
     }
 }
