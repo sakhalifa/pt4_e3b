db3797a100b74be871d55bdc0b1dd17b9ef9a027
manboisson <manon.boisson.1@iut.u-bordeaux.fr>
2022-03-18 11:45:08 +0100

    dernières modifications interfaces

--- PT4/PT4/CreerPrescription.Designer.cs
+++ PT4/PT4/CreerPrescription.Designer.cs
@@ -73,6 +73,7 @@ namespace PT4
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(471, 109);
             this.label3.Name = "label3";
